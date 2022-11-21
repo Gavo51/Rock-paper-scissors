a8125237e03bf6accc96ae693674cacea654979e
Gavo51 <gavo.gs64@gmail.com>
2022-11-21 12:02:53 -0400

    Edit javascript.js
    Added updateScoreboard() wich add a scoreboard and ends the game

--- javascript.js
+++ javascript.js
@@ -1,12 +1,49 @@
+let playerScore=0;
+let computerScore=0;
+
 
 const buttons = document.querySelectorAll(".btn");
 const roundResult = document.querySelector(".results-box");
 const cpuDisplay = document.querySelector(".cpu-display");
+const scoreboard = document.querySelector(".scoreboard");
+const container = document.querySelector(".container");
 
-buttons.forEach(button => { button.addEventListener('click',playRound);});
+buttons.forEach(function(button){ 
+
+    if(computerScore<5 || playerScore<5){
+        button.addEventListener('click',playRound);
+    } else {
+        if(playerScore === 5){
+            scoreboard.textContent = 'THE PLAYER WINS!';
+        } else if(computerScore === 5){
+            scoreboard.textContent = 'THE CPU WINS!';       
+        }
+    }
+
+});
 
 
 
+function updateScoreboard() {
+
+    if(playerScore == 5){
+        scoreboard.textContent = `THE PLAYER WINS! FINAL SCORES >> PLAYER = ${playerScore} || CPU = ${computerScore}`;    
+        buttons.forEach(button => { button.remove();});
+        cpuDisplay.remove();
+        doc
+        return;
+    } else if(computerScore == 5){
+        scoreboard.textContent = `THE CPU WINS! FINAL SCORES >> PLAYER = ${playerScore} || CPU = ${computerScore}`;              
+        buttons.forEach(button => { button.remove();});
+        cpuDisplay.remove();
+        return;
+    } else {
+        scoreboard.textContent = `PLAYER = ${playerScore} || CPU = ${computerScore}`;
+        scoreboard.textContent = `PLAYER = ${playerScore} || CPU = ${computerScore}`;
+        return;
+    }
+   
+}
 
 // generate random number between 0-2 and return rock,paper or scissors 
 function getComputerChoice() {
@@ -37,10 +74,9 @@ function playRound(e) {
 
     cpuDisplay.textContent = computerSelection.toUpperCase();
 
-    console.log(`Player = ${playerSelection} || Computer = ${computerSelection}`);
-
     if(playerSelection===computerSelection){
         roundResult.textContent=`It's a tie! ${playerSelection} is equal to ${computerSelection}`;
+        updateScoreboard();
         return;
     } else {
   
@@ -49,10 +85,14 @@ function playRound(e) {
                 switch(computerSelection) {
                     case 'paper':
                         roundResult.textContent=`You lose! ${computerSelection} beats ${playerSelection}`;
+                        computerScore++;
+                        updateScoreboard();
                         return 0;
                     break;
                     case 'scissors':
                         roundResult.textContent=`You win! ${playerSelection} beats ${computerSelection}`;
+                        playerScore++;
+                        updateScoreboard();
                         return 1;
                     break;
                 }
@@ -62,10 +102,14 @@ function playRound(e) {
                 switch(computerSelection) {
                     case 'rock':
                         roundResult.textContent=`You win! ${playerSelection} beats ${computerSelection}`;
+                        playerScore++;
+                        updateScoreboard();
                         return 1;
                     break;                  
                     case 'scissors':
                         roundResult.textContent=`You lose! ${computerSelection} beats ${playerSelection}`;
+                        computerScore++;
+                        updateScoreboard();
                         return 0;
                     break;
                 }
@@ -75,10 +119,14 @@ function playRound(e) {
                 switch(computerSelection) {
                     case 'rock':
                         roundResult.textContent=`You lose! ${computerSelection} beats ${playerSelection}`;
+                        computerScore++;
+                        updateScoreboard();
                         return 0;
                     break;
                     case 'paper':
                         roundResult.textContent=`You win! ${playerSelection} beats ${computerSelection}`;
+                        playerScore++;
+                        updateScoreboard();
                         return 1;
                     break;
                 }
@@ -97,9 +145,9 @@ function game() {
     let playerScore = 0;
     let computerScore = 0;
 
-//    for (let i=1; i<=5; i++){
+    for (let i=1; i<=5; i++){
 
-//      console.log(`ROUND ${i} !!`);
+        console.log(`ROUND ${i} !!`);
 
         const playerSelection = prompt('Choose rock,paper or scissors:').toLowerCase();
         const computerSelection = getComputerChoice();
@@ -112,8 +160,7 @@ function game() {
             computerScore++;
         }
 
-
-//   }   
+   }   
 
     console.log(`FINAL SCORES: PLAYER = ${playerScore} || COMPUTER=${computerScore}`);
 
